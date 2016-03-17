58d76b957def5060f98c73def27d73d9395e1299
Anonymous Candidate <anon@example.com>
2016-03-17 01:05:35 +0000

    Update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
 (defproject todo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Candidate assignment for a democracy.works job application"
   :dependencies [[org.clojure/clojure "1.8.0"]])
