ca310449d67309058291b4214a26c1d8ca16bf08
Anonymous Candidate <anon@example.com>
2016-03-18 19:26:05 +0000

    Add dummy items to the database when the application starts

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -6,16 +6,19 @@
 
 (defn -main
   [& args]
-  ; db-url and schema-path could use environment variables but for simplicity
-  ; we’ll hardcode.
+  ; Some of these could use environment variables but for simplicity we’ll
+  ; hardcode.
   (let [db-uri "datomic:mem://todo"
         schema-path "datomic/todo-schema.edn"
+        data-path "datomic/todo-data.edn"
         dev-mode? (not (System/getenv "LEIN_NO_DEV"))]
     ; Create the Datomic database anew since it is in-memory.
     (d/create-database db-uri)
     (let [conn (d/connect db-uri)
           system {:datomic conn}]
-      (db/install-schema! conn (db/load-schema schema-path))
+      (db/install-schema! conn (db/read-schema schema-path))
+      (doseq [d (db/read-data data-path)]
+        (db/create-item! conn d))
       (if dev-mode?
         (web/run-dmc (route/app-routes system))
         (web/run (route/app-routes system))))))
--- src/todo/db.clj
+++ src/todo/db.clj
@@ -4,10 +4,21 @@
             [datomic.api :as d])
   (:import [datomic Util]))
 
-(defn load-schema
+(defrecord Item [text state])
+
+(def item-states
+  {:todo :item.state/todo
+   :done :item.state/done})
+
+(defn read-schema
   "Loads the schema EDN file relative to the resource path."
   [path]
   (-> path io/resource io/reader Util/readAll))
+
+(defn read-data
+  "Loads the data EDN file relative to the resource path."
+  [path]
+  (-> path io/resource slurp edn/read-string))
 
 (defn install-schema!
   "Installs the elements of the schema and returns a seq of the return values of
@@ -16,3 +27,10 @@
   (doall
     (for [s schema]
       @(d/transact conn s))))
+
+(defn create-item!
+  "Add a to-do list item to the database."
+  [conn item]
+  @(d/transact conn [{:db/id #db/id[:db.part/user]
+                      :item/text (:text item)
+                      :item/state {:db/ident (item-states (:state item))}}]))
